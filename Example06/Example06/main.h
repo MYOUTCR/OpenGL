679e7851d95c13336e3383706bf7fcb6871a4053
myoutcrx <cn_yhm@163.com>
2018-08-16 00:25:59 +0800

    GLEW配置

---
+++ Example06/Example06/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef _HEAD_EXAMPLE_SIX_MAIN
+#define _HEAD_EXAMPLE_SIX_MAIN
+
+#define GLEW_STATIC    
+#include <GL/glew.h>    
+// GLFW    
+#include <GLFW/glfw3.h> 
+
+
+
+
+// Function prototypes    
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+#endif // !_HEAD_EXAMPLE_SIX_MAIN
